1032a2fbe1923cbef6e4c58e1f10c18e11f9a625
Rares Ghilvacs <rghilvacs@salesforce.com>
2020-05-25 17:18:05 +0300

    Make helper function for render and add default props

--- src/index.js
+++ src/index.js
@@ -18,14 +18,22 @@ class App extends React.Component {
         )
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />;
         }
-        return <Spinner />;
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
